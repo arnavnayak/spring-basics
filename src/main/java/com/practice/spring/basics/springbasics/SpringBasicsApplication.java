24b5eddc240f5151045e9b2129acf09a43961558
Arnav <neilmanoj@gmail.com>
2021-11-04 14:21:07 +0530

    First commit after correcting repo

--- src/main/java/com/practice/spring/basics/springbasics/SpringBasicsApplication.java
+++ src/main/java/com/practice/spring/basics/springbasics/SpringBasicsApplication.java
@@ -1,5 +1,6 @@
 package com.practice.spring.basics.springbasics;
 
+<<<<<<< HEAD
 import com.practice.spring.basics.springbasics.basic.BinarySearchImprovedAgain;
 import com.practice.spring.basics.springbasics.basic.BinarySearchImprovedAgainSetterInjection;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -9,6 +10,16 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan
+=======
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import com.practice.spring.basics.springbasics.basic.BinarySearchImprovedAgain;
+import com.practice.spring.basics.springbasics.basic.BinarySearchImprovedAgainSetterInjection;
+
+@SpringBootApplication
+>>>>>>> 2b69b1d01ac85f3ee2d503cb39b726ef5b3fe070
 public class SpringBasicsApplication {
 
 	//What are the beans ?
@@ -24,7 +35,11 @@ public class SpringBasicsApplication {
 //		BinarySearchImproved bSearch =new BinarySearchImproved(new QuickSortAlgo());// new way with loosely coupled improved way
 		// still the above can be made to instantiate bean wihtout even doing it as above so as to make it more loosely copupled
 		
+<<<<<<< HEAD
 		ConfigurableApplicationContext springApplicationContext = new AnnotationConfigApplicationContext(SpringBasicsApplication.class);
+=======
+		ConfigurableApplicationContext springApplicationContext = SpringApplication.run(SpringBasicsApplication.class, args);
+>>>>>>> 2b69b1d01ac85f3ee2d503cb39b726ef5b3fe070
 		
 		BinarySearchImprovedAgain bSearch =  springApplicationContext.getBean(BinarySearchImprovedAgain.class);
 		
--- src/main/java/com/practice/spring/basics/springbasics/SpringBasicsCdiApplication.java
+++ src/main/java/com/practice/spring/basics/springbasics/SpringBasicsCdiApplication.java
@@ -1,5 +1,6 @@
 package com.practice.spring.basics.springbasics;
 
+<<<<<<< HEAD
 import com.practice.spring.basics.springbasics.cdi.SomeCdiBusiness;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,13 +11,29 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan
+=======
+import com.practice.spring.basics.springbasics.basic.BinarySearchImprovedAgainSetterInjection;
+import com.practice.spring.basics.springbasics.cdi.SomeCdiBusiness;
+import com.practice.spring.basics.springbasics.scope.PersonDAO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+>>>>>>> 2b69b1d01ac85f3ee2d503cb39b726ef5b3fe070
 public class SpringBasicsCdiApplication {
 
 	private static Logger LOGGER = LoggerFactory.getLogger(SpringBasicsCdiApplication.class);
 	
 	public static void main(String[] args) {
 		
+<<<<<<< HEAD
 		ConfigurableApplicationContext springApplicationContext = new AnnotationConfigApplicationContext(SpringBasicsCdiApplication.class);
+=======
+		ConfigurableApplicationContext springApplicationContext = SpringApplication.run(SpringBasicsCdiApplication.class, args);
+>>>>>>> 2b69b1d01ac85f3ee2d503cb39b726ef5b3fe070
 
 		SomeCdiBusiness someCdiBusiness = springApplicationContext.getBean(SomeCdiBusiness.class);
 
